e5d1feededa774e7b6501b3733cdb7e33a0b7f95
selvakumar <selvakumar.k@gmail.com>
2024-03-13 15:49:58 +0530

    Project done

--- src/test/java/runner/LoginTestRunner.java
+++ src/test/java/runner/LoginTestRunner.java
@@ -11,7 +11,8 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class) //Used to report as Cucumber
 @CucumberOptions(features="C:\\Users\\selva\\OneDrive\\Desktop\\SeleniumS2\\CucumberS2\\src\\test\\java\\feature\\Login.feature", 
 glue= {"stepDefintion"},
-plugin= {"html:target/cucumberreport.html"})
+plugin= {"html:target/cucumberreport.html",
+		"json:target/cucumber.json"})
 
 
 public class LoginTestRunner {
--- src/test/java/runner/SearchTestRunner.java
+++ src/test/java/runner/SearchTestRunner.java
@@ -12,7 +12,9 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class) //Used to report as Cucumber
 @CucumberOptions(features="C:\\Users\\selva\\OneDrive\\Desktop\\SeleniumS2\\CucumberS2\\src\\test\\java\\feature\\Search.feature", 
 glue= {"stepDefintion"},
-plugin= {"html:target/cucumberreportSearch.html"})
+plugin= {"html:target/cucumberreportSearch.html",
+		"json:target/cucumber.json"	
+})
 
 
 public class SearchTestRunner {
